645057d6a7001fa14b47c9b52eee1120bf2acc64
hanyun <1355081829@qq.com>
2026-01-02 22:46:33 +0800

    修改类型

--- koi-server/src/constants/index.ts
+++ koi-server/src/constants/index.ts
@@ -1,4 +1,4 @@
 export const jwtConstants = {
   secret:
-    'atYSRR{m;4B0`HlQWkDepbJAuZ+A)EFKrVys} 0o?Mp#{]Mz:VE!f7VcqTM8|&cX`xlij-C Y3NHP{g[>BY7s2`]<Nrkn}(ff ;f',
+    'sA9!oF6|vJ1/aF0+lE5^aC3#qT6+oE5[qD9@pC5&lY6;pZ4{iN5,iC4+gE3$mJ9(gN0(sM1}aP4%mQ2`rX5#cE0#oV3>bS0@wX3$qJ3`pM0*gO9=zU8(fF9&tP0^oY9~',
 };
