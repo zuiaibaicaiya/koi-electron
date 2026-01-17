15bc7e3bc0b7ebed52424a115f792537de152c94
hanyun <1355081829@qq.com>
2026-01-17 21:28:11 +0800

    增加闪屏页

--- koi-server/src/app.controller.ts
+++ koi-server/src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { success } from '@/utils/response';
+import { ApiSuccessResponse } from '@/decorators/api-success-response/api-success-response.decorator';
+import { Public } from '@/decorators/isPublic';
 
+@ApiTags('app')
+@Public()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOperation({
+    summary: 'ping',
+  })
+  @ApiSuccessResponse()
+  @Get('/ping')
+  ping() {
+    return success();
   }
 }
