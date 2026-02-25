bae5ab28351359d6f3bce2a94eb91cb2fc5bfc19
hanyun <1355081829@qq.com>
2026-02-25 22:09:18 +0800

    升级依赖

--- koi-ui/src/pages/user/UserPage.vue
+++ koi-ui/src/pages/user/UserPage.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { ProTable } from '@/components/ProTable/src';
 import useTable from '@/components/ProTable/src/hooks/useTable.ts';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { getUserList } from '@/api/user.ts';
+import type { TableProps } from 'antdv-next';
 const columns = ref([
   {
     title: 'id',
@@ -24,11 +25,28 @@ const [register, { reload }] = useTable({
   columns,
   api: getUserList,
 });
+const checkStrictly = ref(false);
+
+const rowSelection = computed<TableProps['rowSelection']>(() => ({
+  checkStrictly: checkStrictly.value,
+  onChange: (selectedRowKeys, selectedRows, info) => {
+    console.log(
+      `selectedRowKeys: ${selectedRowKeys}`,
+      'selectedRows: ',
+      selectedRows,
+      'info',
+      info,
+    );
+  },
+  onSelect: (record, selected, selectedRows) => {
+    console.log(record, selected, selectedRows);
+  },
+}));
 </script>
 
 <template>
   <div>
-    <pro-table @register="register" rowKey="id">
+    <pro-table @register="register" rowKey="id" :row-selection="rowSelection">
       <template #title>
         <a-button type="primary" @click="reload">reload</a-button>
       </template>
